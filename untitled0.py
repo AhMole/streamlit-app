757e1761f10520da1b52e02ed27955c49278b3fa
AhMole <61013003+AhMole@users.noreply.github.com>
2022-11-27 00:09:24 +0100

    Update untitled0.py

--- untitled0.py
+++ untitled0.py
@@ -12,7 +12,7 @@ import streamlit as st
 
 #df = pd.read_csv(r'/Users/alexander/Downloads/sales_data_sample.csv', encoding = "ISO-8859-1")
 
-print(df)
+
 
 st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
 
